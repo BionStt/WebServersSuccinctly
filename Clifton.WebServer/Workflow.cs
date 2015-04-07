95abf2b09d87f4a69ab45a4a4d9fb1ea67090b82
Sander Rossel <sander.rossel@gmail.com>
2015-04-07 19:11:35 +0200

    Fixed #1 - NullReference
    Setting the AbortHandler in the Workflow constructor.

--- Clifton.WebServer/Workflow.cs
+++ Clifton.WebServer/Workflow.cs
@@ -79,6 +79,7 @@ namespace Clifton.WebServer
 		public Workflow(Action<T> abortHandler, Action<T, Exception> exceptionHandler)
 		{
 			items = new List<WorkflowItem<T>>();
+            AbortHandler = abortHandler;
 			ExceptionHandler = exceptionHandler;
 		}
 
